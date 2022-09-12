e4b23220f45124ae85bb5fbf6e19886f1b561a99
Murilo <murilomurilomarquesmurilo@hotmail.com>
2022-09-12 11:18:43 -0300

    aaa

---
+++ programa.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int n1, result;
+    printf("Escreva sua idade e diremos quantos anos tera em 2030 ");
+    scanf("%d", &n1);
+    result = n1 + 8;
+    printf("Sua idade em 2030 será de %i", result);
+}
